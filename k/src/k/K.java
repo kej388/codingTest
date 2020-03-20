c62b5d6602d00806ae1b8170068140adf7df103b
kej388 <eunjinkoh96@gmail.com>
2020-03-20 18:18:32 +0900

    k번째수

---
+++ k/src/k/K.java
@@ -0,0 +1,38 @@
+package k;
+
+import java.util.Arrays;
+
+class Solution {
+	
+	private static final String ArrayList = null;
+
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        
+        // array의 i번째 숫자
+        
+        
+        
+        return answer;
+    }
+}
+
+public class K {
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = { 
+				{2, 5, 3},
+				{4, 4, 1},
+				{1, 7 ,3}
+		};
+		
+		int[] result = s.solution(array, commands);
+		
+		System.out.println(result);
+		
+	}
+
+}
