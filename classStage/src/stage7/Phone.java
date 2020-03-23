f8777ac824f1d04a54a83df21720eba28ee5c7b8
kej388 <eunjinkoh96@gmail.com>
2020-03-23 18:21:22 +0900

    클래스문제(stage7)

---
+++ classStage/src/stage7/Phone.java
@@ -0,0 +1,11 @@
+package stage7;
+
+public class Phone {
+	String name;
+	String tel;
+	
+	public void set(String name, String tel) {
+		this.name = name;
+		this.tel = tel;
+	}
+}
---
+++ classStage/src/stage7/PhoneBook.java
@@ -0,0 +1,24 @@
+package stage7;
+
+public class PhoneBook {
+	
+	public void input() {
+		
+	}
+	
+	public void search() {
+		
+	}
+	
+	public void finish() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public static void main(String[] args) {
+		PhoneBook pb = new PhoneBook();
+	}
+}
