da41d136ebcc36e767713ab7f1de4ee701819f77
kej388 <eunjinkoh96@gmail.com>
2020-03-23 18:21:59 +0900

    상속 문제(stage3)

---
+++ ExtendsStage/src/stage3/ColorTV.java
@@ -0,0 +1,16 @@
+package stage3;
+
+public class ColorTV extends TV{
+	
+	private int color;
+	
+	public ColorTV(int size, int color) {
+		super(size);
+		this.color = color;
+	}
+	
+	public void printProperty() {
+		
+		System.out.println();
+	}
+}
---
+++ ExtendsStage/src/stage3/ViewTV.java
@@ -0,0 +1,9 @@
+package stage3;
+
+public class ViewTV {
+	
+	public static void main(String[] args) {
+		ColorTV myTV = new ColorTV(32, 1024);
+		myTV.printProperty();
+	}
+}
