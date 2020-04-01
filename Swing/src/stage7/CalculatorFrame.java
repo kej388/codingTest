4f07f066383c77acadcfe48ba303670edd3b845a
kej388 <eunjinkoh96@gmail.com>
2020-04-01 18:18:02 +0900

    계산기GUI 완성

--- Swing/src/stage7/CalculatorFrame.java
+++ Swing/src/stage7/CalculatorFrame.java
@@ -1,10 +1,15 @@
 package stage7;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalculatorFrame extends JFrame{
 	
@@ -24,19 +29,41 @@ public class CalculatorFrame extends JFrame{
 	
 	class NorthPanel extends JPanel {
 		public NorthPanel() {
-			
+			setBackground(Color.GRAY);
+			JLabel text = new JLabel("수식입력");
+			JTextField input = new JTextField(10);
+			add(text);
+			add(input);
 		}
 	}
 	
 	class CenterPanel extends JPanel {
 		public CenterPanel() {
-			
+			setLayout(new GridLayout(4, 4));
+			add(new JButton("0"));
+			add(new JButton("1"));
+			add(new JButton("2"));
+			add(new JButton("3"));
+			add(new JButton("4"));
+			add(new JButton("5"));
+			add(new JButton("6"));
+			add(new JButton("7"));
+			add(new JButton("8"));
+			add(new JButton("9"));
+			add(new JButton("CE"));
+			add(new JButton("계산"));
+			add(new JButton("+")).setBackground(Color.CYAN);
+			add(new JButton("-")).setBackground(Color.CYAN);
+			add(new JButton("*")).setBackground(Color.CYAN);
+			add(new JButton("/")).setBackground(Color.CYAN);
 		}
 	}
 	
 	class SouthPanel extends JPanel {
 		public SouthPanel() {
-			
+			setBackground(Color.YELLOW);
+			add(new JLabel("계산 결과"));
+			add(new JTextField(10));
 		}
 	}
 	
